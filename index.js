7682d8ceb01ddca09271209f8a51131e9242b281
j00009 <joeltroncoso2002@gmail.com>
2025-02-19 19:12:44 -0300

    go produccion

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { v4: uuidv4 } = require("uuid");
 const Jimp = require("jimp");
+const fs = require('fs');
 
 const app = express()
 
@@ -8,8 +9,8 @@ app.use("/bootstrap", express.static("node_modules/bootstrap/dist"));
 app.use("/popper", express.static("node_modules/@popperjs/core/dist/umd"));
 app.use(express.static('public'))
 
-app.listen(3000, () => {
-  console.log("App en puerto 3000")
+app.listen(3006, () => {
+  console.log("App en puerto 3006")
 })
 
 app.get("/", (req, res) => {
@@ -20,12 +21,16 @@ app.get("/foto", async (req, res) => {
   const { url_imagen } = req.query
   const id = uuidv4().slice(0, 6)
 
+  const photosDir = __dirname + "/public/photos/";
+  if (!fs.existsSync(photosDir)) {
+    fs.mkdirSync(photosDir, { recursive: true });
+  }
 
   const img = await Jimp.read(url_imagen)
 
   await img.grayscale()
           .resize(350, Jimp.AUTO)
-          .writeAsync("./" + id + ".jpg")
+          .writeAsync(photosDir + id + ".jpg")
 
-  res.sendFile( __dirname + "/"+ id + ".jpg")
+  res.sendFile(photosDir + id + ".jpg")
 })
